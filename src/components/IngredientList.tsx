d2a012b1b6d3e4baf0a6cf5911e7dc79cd8e22dc
Chris Emerson <chris.emerson@roledex.com>
2018-08-24 15:47:00 -0600

    Main component stores input value in state
    Signed-off-by: Chris Emerson <chris.emerson@roledex.com>

--- src/components/IngredientList.tsx
+++ src/components/IngredientList.tsx
@@ -4,7 +4,7 @@ import './IngredientList.css';
 export const IngredientList = (props : {ingredients: Array<{name: string}>}) => (
     <ul className={'ingredient-list'}>
         {props.ingredients.map(ingredient => (
-            <li className={''}>
+            <li className={''} key={ingredient.name}>
                 <h2>{ingredient.name}</h2>
             </li>
         ))}
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,9 +2,32 @@ import * as React from 'react';
 import ingredients from "../data/mock-data";
 import {IngredientList} from "./IngredientList";
 
-export const Main = () => (
-    <main>
-        <IngredientList ingredients={ingredients} />
-        <input type={'text'} placeholder={'Ingredient'} />
-    </main>
-);
+export class Main extends React.Component<{}, {filterString: string}> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            filterString: ''
+        }
+    }
+
+    public handleInput = (event: any) => {
+        const filterString = event.target.value;
+        this.setState({
+            filterString
+        });
+    };
+
+    public render() {
+        return (
+            <main>
+                <IngredientList ingredients={ingredients} />
+                <input
+                    type={'text'}
+                    placeholder={'Ingredient'}
+                    onChange={this.handleInput}
+                    value={this.state.filterString}
+                />
+            </main>
+        )
+    }
+}
