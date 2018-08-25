3e2707b6f9bea96c867f7b9f3f211b783a334a33
Chris Emerson <chris.emerson@roledex.com>
2018-08-24 22:03:03 -0600

    Ingredients toggle effects list on click
    Signed-off-by: Chris Emerson <chris.emerson@roledex.com>

--- src/apply-filters.test.ts
+++ src/apply-filters.test.ts
@@ -1,26 +1,26 @@
 import { applyFilters } from './apply-filters';
 
 test('no filters returns all ingredients', () => {
-    const ingredients = [{name: 'a'}, {name: 'b'}];
+    const ingredients = [{effects: [], name: 'a'}, {effects: [], name: 'b'}];
 
     expect(applyFilters(ingredients).length).toBe(ingredients.length);
 });
 
 test('works with a single filter', () => {
-    const ingredients = [{name: 'a'}, {name: 'b'}, {name: 'c'}];
+    const ingredients = [{effects: [], name: 'a'}, {effects: [], name: 'b'}, {effects: [], name: 'c'}];
 
     const aFilter = ({name}: {name: string}) => (name === 'a');
 
     expect(applyFilters(ingredients, aFilter))
-        .toEqual([{name: 'a'}]);
+        .toEqual([{effects: [], name: 'a'}]);
 });
 
 test('works with multiple filters', () => {
-    const ingredients = [{name: 'a'}, {name: 'b'}, {name: 'c'}];
+    const ingredients = [{effects: [], name: 'a'}, {effects: [], name: 'b'}, {effects: [], name: 'c'}];
 
     const not_aFilter = ({name}: {name: string}) => (name !== 'a');
     const not_bFilter = ({name}: {name: string}) => (name !== 'b');
 
     expect(applyFilters(ingredients, not_aFilter, not_bFilter))
-        .toEqual([{name: 'c'}]);
+        .toEqual([{effects: [], name: 'c'}]);
 });
--- src/components/IngredientItem.tsx
+++ src/components/IngredientItem.tsx
@@ -1,8 +1,33 @@
 import * as React from 'react';
 import {Ingredient} from "../models/Ingredient.model";
 
-export const IngredientItem = ({ingredient}: {ingredient: Ingredient}) => (
-    <li className={'ingredient-item'}>
-        <h2>{ingredient.name}</h2>
-    </li>
-);
+export class IngredientItem extends React.Component<{ingredient: Ingredient}, {expanded: boolean}> {
+    constructor(props: {ingredient: Ingredient}) {
+        super(props);
+        this.state = {
+            expanded: false
+        };
+    }
+
+    public render() {
+        const {ingredient} = this.props;
+        const {expanded} = this.state;
+        return (
+            <li className={'ingredient-item'} onClick={this.handleClick}>
+                <h2>{ingredient.name}</h2>
+                {
+                    expanded
+                        ? ingredient.effects.map(effect => <p key={effect}>{effect}</p>)
+                        : null
+                }
+            </li>
+        );
+    }
+
+    private handleClick = () => {
+        const {expanded} = this.state;
+        this.setState({
+            expanded: !expanded
+        });
+    };
+}
