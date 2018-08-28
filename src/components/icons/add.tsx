35aaa23c737585e25b70b516240d2fe15bbc2c65
Chris Emerson <chris.emerson@roledex.com>
2018-08-27 23:11:18 -0600

    Improve prop destructuring in icon component
    Signed-off-by: Chris Emerson <chris.emerson@roledex.com>

--- src/components/icons/add.tsx
+++ src/components/icons/add.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 
-export const AddIcon = (props: {style: {backgroundColor: string, color: string}}) => {
-    const {style} = props;
+export const AddIcon = ({style}: {style: {backgroundColor: string, color: string}}) => {
+    const {backgroundColor, color} = style;
     return (
         <svg width={20} height={20}>
-            <circle cx={10} cy={10} r={10} fill={style.backgroundColor}/>
+            <circle cx={10} cy={10} r={10} fill={backgroundColor}/>
             <text
                 x={10} y={15.5}
                 fontSize={20}
                 textAnchor={'middle'}
-                fill={style.color}
+                fill={color}
                 fontWeight={'bold'}
             >+</text>
         </svg>
