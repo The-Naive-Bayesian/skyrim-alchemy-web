000ce1317ccb92f2ed781e6ea192b06d213bae1f
Chris Emerson <cemerson@i3logix.com>
2018-08-28 11:36:09 -0600

    Filter ingredients to those sharing selected ingredient effect(s)
    Signed-off-by: Chris Emerson <cemerson@i3logix.com>

--- src/components/IngredientFilter.tsx
+++ src/components/IngredientFilter.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {applyFilters} from "../apply-filters";
 import ingredients from "../data/ingredients";
-import {nameContains, nameDoesNotContain} from "../filters";
+import {nameContains, nameDoesNotContain, sharesEffect} from "../filters";
 import {Ingredient} from "../models/Ingredient.model";
 
 // Define prop types
@@ -14,7 +14,16 @@ interface IProps {
 export const IngredientProvider = ({filterString, selectedIngredients, render}: IProps) => {
     const nameFilter = nameContains(filterString);
     const ingredientNameFilters = selectedIngredients.map(ingredient => nameDoesNotContain(ingredient.name));
-    const ingredientsToRender = applyFilters(ingredients, nameFilter, ...ingredientNameFilters);
+    const additionalFilters = [];
+    if (selectedIngredients.length > 0) {
+        additionalFilters.push(sharesEffect(selectedIngredients));
+    }
+    const ingredientsToRender = applyFilters(
+        ingredients,
+        nameFilter,
+        ...ingredientNameFilters,
+        ...additionalFilters
+    );
     return (
         <>
             {render(ingredientsToRender)}
